7f3cb824afeeb5d9cda1c4acda375a38e7cb3557
Jay254 <jyegon504@gmail.com>
2023-03-14 14:54:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random =Math.ceil(Math.random() * 10);
+console.log(random);
+
+let num3=9;
+let num4=5;
+let mod = num3%num4;
+console.log(mod);
+
+let set = (1,5,9,20);
+let max = Math.max(set);
+console.log(max);
